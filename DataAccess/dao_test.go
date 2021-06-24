8faa157fe1854a2293efd4ae71a97f10854856f2
pranjal-tb <pranjal@thinkbridge.in>
2021-06-24 13:57:28 +0530

    Change in test hierarchy

--- DataAccess/dao_test.go
+++ DataAccess/dao_test.go
@@ -11,13 +11,7 @@ import (
 )
 
 //Test authentication with valid Credentials
-func Test_CheckUser_With_ValidCredentials(t *testing.T) {
-	//Sample Valid Creds
-	creds := &dto.LoginCredentials{
-		Email:    "ajith@thinkbridge.in",
-		Password: "Ajith12#",
-	}
-
+func Test_CheckUser(t *testing.T) {
 	//Mock Instance of sql.DB
 	mock_db, mock, err := sqlmock.New()
 	if err != nil {
@@ -38,53 +32,40 @@ func Test_CheckUser_With_ValidCredentials(t *testing.T) {
 	//Our dataAccess operator, uses sqlX.DB
 	mock_dao := &dao{db: mock_xdb}
 	//Call for credential match into database
-	isAuthenticated, _ := mock_dao.CheckUser(creds.Email, creds.Password)
 
-	//Should Authenticate
-	if !isAuthenticated {
-		t.Fatalf(`Failed to Authenticate`)
-	}
+	t.Run("validCredentials", func(t *testing.T) {
+		//Sample Valid Creds
+		creds := &dto.LoginCredentials{
+			Email:    "ajith@thinkbridge.in",
+			Password: "Ajith12#",
+		}
+		isAuthenticated, _ := mock_dao.CheckUser(creds.Email, creds.Password)
 
-	if eror := mock.ExpectationsWereMet(); eror != nil {
-		t.Fatalf(eror.Error())
-	}
-}
+		//Should Authenticate
+		if !isAuthenticated {
+			t.Fatalf(`Failed to Authenticate`)
+		}
 
-//Test authentication with Invalid Credentials
-func Test_CheckUser_With_InvalidCredentials(t *testing.T) {
-	//Sample Valid Creds
-	creds := &dto.LoginCredentials{
-		Email:    "notajith@thinkbridge.in",
-		Password: "Ajith12#",
-	}
+		if eror := mock.ExpectationsWereMet(); eror != nil {
+			t.Fatalf(eror.Error())
+		}
+	})
 
-	//Mock Instance of sql.DB
-	mock_db, mock, err := sqlmock.New()
-	if err != nil {
-		fmt.Println("expected no error, but got:", err)
-		return
-	}
-	defer mock_db.Close()
+	t.Run("invalidCredentials", func(t *testing.T) {
+		//Sample Inalid Creds
+		creds := &dto.LoginCredentials{
+			Email:    "notajith@thinkbridge.in",
+			Password: "Ajith12#",
+		}
+		isAuthenticated, _ := mock_dao.CheckUser(creds.Email, creds.Password)
 
-	//sqlX.DB instance with core as a mocked sql.DB
-	mock_xdb := sqlx.NewDb(mock_db, "sqlserver")
+		//Should not authenticate!
+		if isAuthenticated {
+			t.Fatalf(`Shouldn't have authenticated`)
+		}
 
-	//Emulated output (No rows at all)
-	rows := sqlmock.NewRows([]string{"NaesbUserKey", "Name", "Email"})
-	//Query to expect and then emulate output
-	mock.ExpectQuery(regexp.QuoteMeta("select cast(NaesbUserKey as char(36)) as NaesbUserKey, Name, Email from NaesbUser where Email=@p1 and Password=@p2")).WillReturnRows(rows)
-
-	//Our dataAccess operator, uses sqlX.DB
-	mock_dao := &dao{db: mock_xdb}
-	//Call for credential match into database
-	isAuthenticated, _ := mock_dao.CheckUser(creds.Email, creds.Password)
-
-	//Should not authenticate!
-	if isAuthenticated {
-		t.Fatalf(`WHAT`)
-	}
-
-	if eror := mock.ExpectationsWereMet(); eror != nil {
-		t.Fatalf(eror.Error())
-	}
+		if eror := mock.ExpectationsWereMet(); eror != nil {
+			t.Fatalf(eror.Error())
+		}
+	})
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module tinc1
 go 1.16
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/denisenkom/go-mssqldb v0.10.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.2
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jmoiron/sqlx v1.3.4
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
 )
