447d4613f8ba54c27eb10877ce10c07a09eb5c90
pranjal-tb <pranjal@thinkbridge.in>
2021-07-01 15:11:09 +0530

    Added Test Case
    In Services/jwt_service_test > TestValidateToken/InvalidSigningMethod

--- Services/jwtservice_test.go
+++ Services/jwtservice_test.go
@@ -2,6 +2,9 @@ package services
 
 import (
 	"testing"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 func TestGenerateToken(t *testing.T) {
@@ -49,7 +52,7 @@ func TestValidateToken(t *testing.T) {
 		}
 
 		//Generate token based on sample claim
-		sample_token := mock_jwtService.GenerateToken("sample.email.com")
+		sample_token := mock_jwtService.GenerateToken("sample@email.com")
 
 		//Try to validate the token
 		_, err := NewJWTService().ValidateToken(sample_token)
@@ -57,6 +60,37 @@ func TestValidateToken(t *testing.T) {
 		//Validation should err
 		if err == nil {
 			t.Fatal(`should've been error for invalid key`)
+		}
+	})
+
+	t.Run("InvalidSigningMethod", func(t *testing.T) {
+		mock_jwtService := &jwtService{
+			secretKey: "UjgFm344XW",
+			issuer:    "thinkbridgeIdProvider",
+		}
+
+		//Set sample claims
+		claims := &jwtCustomClaims{
+			"sample@email.com",
+			jwt.StandardClaims{
+				Issuer:    mock_jwtService.issuer,
+				IssuedAt:  time.Now().Unix(),
+				ExpiresAt: time.Now().Add(time.Hour * 1).Unix(),
+			},
+		}
+
+		//Create token with invalid signing method
+		sample_token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+		//Generate encoded token using the secret signing key
+		output, _ := sample_token.SignedString([]byte(mock_jwtService.secretKey))
+
+		//Try to validate the token
+		_, err := NewJWTService().ValidateToken(output)
+
+		//Validation should err
+		if err == nil {
+			t.Fatal(`should've been error for unexpected signing method key`)
 		}
 	})
 }
