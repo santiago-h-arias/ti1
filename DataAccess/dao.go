512445f7cd505f7cc7a5a7c2a108e205711dfeaa
Rajesh Kumar <rajesh@thinkbridge.in>
2021-07-05 18:52:57 +0530

    Added inactive and outbound file id

--- DataAccess/dao.go
+++ DataAccess/dao.go
@@ -58,7 +58,7 @@ func (dao *dao) CheckUser(email string, password string) (bool, models.NaesbUser
 
 func (dao *dao) GetInboundFiles(id string) []models.Inboundfile {
 	var findfiles []models.Inboundfile
-	err := dao.db.Select(&findfiles, "select if2.UsCommonCode, if2.ThemCommonCode , if2.Filename, if2.Plaintext, if2.Ciphertext, if2.ReceivedAt, if2.TransactionId, if2.Processed, if2.InboundFileId, u.Name as UsName, t.Name  as ThemName, cast(nuu.NaesbUserKey as char(36)) as NaesbUserKey, cast(InboundFileKey as char(36)) as InboundFileKey, cast(if2.UsKey as char(36)) as UsKey, cast(if2.ThemKey as char(36)) as ThemKey from InboundFiles if2 left join NaesbUserUs nuu on nuu.UsKey = if2.Uskey  join Us u on u.UsKey = if2.UsKey join Them t on t.ThemKey = if2.ThemKey where nuu.Inactive = 0 and nuu.NaesbUserKey=@p1", id)
+	err := dao.db.Select(&findfiles, "select if2.UsCommonCode, if2.ThemCommonCode , if2.Filename, if2.Plaintext, if2.Ciphertext, if2.ReceivedAt, if2.TransactionId, if2.Processed, if2.InboundFileId, u.Name as UsName, t.Name  as ThemName, nuu.Inactive, cast(nuu.NaesbUserKey as char(36)) as NaesbUserKey, cast(InboundFileKey as char(36)) as InboundFileKey, cast(if2.UsKey as char(36)) as UsKey, cast(if2.ThemKey as char(36)) as ThemKey from InboundFiles if2 left join NaesbUserUs nuu on nuu.UsKey = if2.Uskey  join Us u on u.UsKey = if2.UsKey join Them t on t.ThemKey = if2.ThemKey where nuu.Inactive = 0 and nuu.NaesbUserKey=@p1", id)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -67,7 +67,7 @@ func (dao *dao) GetInboundFiles(id string) []models.Inboundfile {
 
 func (dao *dao) GetOutboundFiles(id string) []models.Outboundfile {
 	var findfiles []models.Outboundfile
-	err := dao.db.Select(&findfiles, "select of2.UsCommonCode, of2.ThemCommonCode, of2.Filename, of2.Plaintext, of2.Ciphertext, of2.Attempt1At, of2.Attempt2At, of2.Attempt3At,of2.Receipt, of2.Result, of2.Escalated , of2.EscalatedAt , of2.Debug , of2.CreatedAt , of2.EmpowerOutgoingEdiFileKey , of2.DoNotSend , of2.LastLocation , of2.Ciphered , of2.Posted, u.Name as UsName, t.Name  as ThemName, cast(nuu.NaesbUserKey as char(36)) as NaesbUserKey, cast(OutboundFileKey as char(36)) as OutboundFileKey, cast(of2.UsKey as char(36)) as UsKey, cast(of2.ThemKey as char(36)) as ThemKey from OutboundFiles of2 left join NaesbUserUs nuu on nuu.UsKey = of2.Uskey join Us u on u.UsKey = of2.UsKey join Them t on t.ThemKey = of2.ThemKey where nuu.Inactive = 0 and nuu.NaesbUserKey =@p1", id)
+	err := dao.db.Select(&findfiles, "select of2.UsCommonCode, of2.ThemCommonCode, of2.Filename, of2.Plaintext, of2.Ciphertext, of2.Attempt1At, of2.Attempt2At, of2.Attempt3At,of2.Receipt, of2.Result, of2.Escalated , of2.EscalatedAt , of2.Debug , of2.CreatedAt , of2.EmpowerOutgoingEdiFileKey , of2.DoNotSend , of2.LastLocation , of2.Ciphered , of2.Posted, of2.OutboundFileId, nuu.Inactive, u.Name as UsName, t.Name  as ThemName, cast(nuu.NaesbUserKey as char(36)) as NaesbUserKey, cast(of2.OutboundFileKey as char(36)) as OutboundFileKey, cast(of2.UsKey as char(36)) as UsKey, cast(of2.ThemKey as char(36)) as ThemKey from OutboundFiles of2 left join NaesbUserUs nuu on nuu.UsKey = of2.Uskey join Us u on u.UsKey = of2.UsKey join Them t on t.ThemKey = of2.ThemKey where nuu.Inactive = 0 and nuu.NaesbUserKey =@p1", id)
 	if err != nil {
 		fmt.Println(err)
 	}
